5d7bcb4a0073ae5dae623c702d4bc803df488efc
camillevasquez <98370142+camillevasquez@users.noreply.github.com>
2022-08-10 16:17:36 -0500

    Add files via upload

---
+++ CloudR_Case_Study/Packages.R
@@ -0,0 +1,12 @@
+## INSTALL PACKAGES:
+install.packages("tidyverse")
+install.packages('ggplot2')
+install.packages("skimr")
+install.packages("janitor")
+install.packages("dplyr")
+
+library(tidyverse)
+library(ggplot2)
+library(skimr)
+library(janitor)
+library(dplyr)
